f02ffe86a1e8911b7a7bb5e483a372db2f91a98e
paras verma <parasverma2531@gmail.com>
2024-05-13 15:36:14 +0530

    verifySchema update

--- src/schemas/verifySchema.ts
+++ src/schemas/verifySchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 // verifySchema validation
 export const verifySchema = z.object({
-  code: z
+  otp: z
     .string()
     .length(6, { message: "Verification code must be of 6 digits" }),
 });
