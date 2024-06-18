bd2945cd53a5ad666246b689fabee2f12b8fba41
paras verma <parasverma2531@gmail.com>
2024-06-18 11:32:34 +0530

    suggestMessage route

--- src/app/api/suggestMessage/route.ts
+++ src/app/api/suggestMessage/route.ts
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -8,14 +8,14 @@ const openai = new OpenAI({
 
 export const runtime = "edge";
 
-export async function POST(req: NextRequest) {
+export async function POST(req: Request) {
   try {
     const prompt =
       "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
     const response = await openai.completions.create({
       model: "gpt-3.5-turbo-instruct",
-      max_tokens: 300,
+      max_tokens: 250,
       stream: true,
       prompt,
     });
@@ -27,7 +27,7 @@ export async function POST(req: NextRequest) {
     if (error instanceof OpenAI.APIError) {
       // OpenAI API error handling
       const { name, status, headers, message } = error;
-      return NextResponse.json({ name, status, headers, message });
+      return NextResponse.json({ name, status, headers, message }, { status });
     } else {
       // General error handling
       console.error("An unexpected error occurred:", error);
